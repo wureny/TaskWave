fda915dcb2bb0b9bf4cf9af05a4ada21ce348470
wureny <wurenyu19@gmail.com>
2024-03-03 15:06:26 +0800

    feat: add taskrpc

--- taskutils/rpc/task_rpc.go
+++ taskutils/rpc/task_rpc.go
@@ -1,1 +1,47 @@
 package rpc
+
+import "github.com/wureny/TaskWave/taskutils/rpc/model"
+
+type TaskRpc struct {
+	Host string
+}
+
+func (t TaskRpc) CreateTask(req *model.CreateTaskReq) (*model.CreateTaskResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) SetTask(req *model.SetTaskReq) (*model.SetTaskResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) HoldTasks(req *model.HoldTasksReq) (*model.HoldTasksResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) GetTaskList(req *model.GetTaskListReq) (*model.GetTaskListResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) GetTask(req *model.GetTaskReq) (*model.GetTaskResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) GetTaskScheduleCfgList() (*model.GetTaskScheduleCfgListResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) GetTaskCountsByType(req *model.GetTaskCountsByTypeReq) (*model.GetTaskCountsByTypeResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t TaskRpc) GetTaskCountsBtStatus(req *model.GetTaskCountsBtStatusReq) (*model.GetTaskCountsBtStatusResp, error) {
+	//TODO implement me
+	panic("implement me")
+}
