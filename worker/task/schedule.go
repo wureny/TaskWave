8fe4b9e5b868f7c6a08d7258e160116b9bb9ec20
wureny <wurenyu19@gmail.com>
2024-03-05 16:57:42 +0800

    feat: add LoadCfg

--- worker/task/schedule.go
+++ worker/task/schedule.go
@@ -1,9 +1,27 @@
 package task
 
-import "github.com/wureny/TaskWave/taskutils/rpc/model"
+import (
+	"github.com/wureny/TaskWave/taskutils/rpc"
+	"github.com/wureny/TaskWave/taskutils/rpc/model"
+)
+
+var taskRpc rpc.TaskRpc
 
 var scheduleCfgDict map[string]*model.TaskScheduleCfg
 
 func init() {
 	scheduleCfgDict = make(map[string]*model.TaskScheduleCfg)
+}
+
+// LoadCfg func load cfg
+func LoadCfg() error {
+	cfgList, err := taskRpc.GetTaskScheduleCfgList()
+	if err != nil {
+		//TODO log
+		return err
+	}
+	for _, cfg := range cfgList.ScheduleCfgList {
+		scheduleCfgDict[cfg.TaskType] = cfg
+	}
+	return nil
 }
